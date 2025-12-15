f33ec2446ba92fcf923384c9fabca13a9d3e687e
SHREYASH SINGHAI <145225995+SHREYASHSINGHAI@users.noreply.github.com>
2025-12-15 23:50:34 +0530

    Refactor imports and reformat code structure

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
-from sklearn.metrics import precision_score,recall_score,f1_score
 from sklearn.metrics import RocCurveDisplay
 
 # importing the dataset 
@@ -303,3 +302,4 @@ print(feature_dict)
 feature_df=pd.DataFrame(feature_dict,index=[0])
 feature_df.T.plot.bar(title='Feature Importance',legend=False)
 plt.show()
+
